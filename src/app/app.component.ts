031d476b9302da47ae8c37f2338fbf3c879c524c
Paulius Dambrauskas <82829703+Paulius101@users.noreply.github.com>
2021-10-24 11:07:49 +0300

    view encapsulation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import {
   ChangeDetectionStrategy,
-  Component
+  Component,
+  ViewEncapsulation
 } from '@angular/core';
 import {
   Produktas
@@ -9,6 +10,7 @@ import {
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+ encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {}
